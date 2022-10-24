9b49c7a50090f4f428d5b9478ece79d08401b2bf
darrenwsu <khanh.truong@wsu.edu>
2022-10-23 21:16:45 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+3 # int - interger
+3.5 # float
+"hello" # str - string
+True # bool
+
+name = "Khanh"
+age = 23
+is_married = False
+
+print(name)
+print(age)
+
+number = input('> ')
+print(number)
